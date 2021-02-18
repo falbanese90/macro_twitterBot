013146d271170e20e046962ec7894abffbbda04e
Frank Albanese <albanesefc9@gmail.com>
2021-02-18 10:38:06 -0500

    Added Countries to headlines

--- twitterBot.py
+++ twitterBot.py
@@ -25,19 +25,29 @@ def scrape():
     soup = BeautifulSoup(html, 'lxml')
     # Extracts all inner html text of headline info
     calendar_events = soup.find_all('a', class_='calendar-event')
+    # Extract list of countries so we can know where headline event takes place
+    countries = soup.find_all('td', class_='calendar-iso')
+    country_list = []
+    for n in countries:
+        country_list.append(n.get_text())
     # writes a text file of list of macro-  economic events
     # taking place this week
+    countries = soup.find_all('td', class_='calendar-iso')
     with open('events.txt', 'w') as events:
+        x = 0
         for n in calendar_events:
-            events.write(n.get_text() + '\n')
+            events.write(country_list[x] + " - " + n.get_text() + '\n')
+            x += 1
     # Selenium closes browser instance
     driver.quit()
 
 
 def tweet():
+    # Activates twitter api with OAuth
     auth = tweepy.OAuthHandler(twitter, twitter_pass)
     auth.set_access_token(twitter_access, twitter_secret)
     api = tweepy.API(auth)
+    # Tweets the written file
     with open('events.txt', 'r') as events:
         api.update_status('Major Macro Events for this week:\n\n' + events.read())
 
