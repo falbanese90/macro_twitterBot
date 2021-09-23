960ce7ada9ddcb79b2baf007b8ff92150f8fa20f
Frank Albanese <>
2021-09-23 15:13:31 -0400

    Latest commit

--- selenium_dr.py
+++ selenium_dr.py
@@ -2,28 +2,30 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
 
+def fetch_events():
+    """Allows selenium to run directly in incognito mode
+    Selenium runs your browser and opens webpage
+    Sets java script variables and executes them on page
+    Extracts source code and makes a Beautiful Soup object
+    writes a text file of list of macro-economic events taking place this week
+    Selenium closes browser instance"""
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--incognito')
+    chrome_options.add_argument('headless')
+    driver = webdriver.Chrome(options=chrome_options)
+    
+    driver.implicitly_wait(2)
+    
+    driver.get('https://tradingeconomics.com/calendar')
+    calendar_range = 'setCalendarRange("3")'
+    calendar_importance = 'setCalendarImportance("3")'
+    driver.execute_script(calendar_range)
+    driver.execute_script(calendar_importance)
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'lxml')
+    calendar_events = soup.find_all('a', class_='calendar-event')
 
-# Allows selenium to run directly in incognito mode
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--incognito')
-chrome_options.add_argument('headless')
-# Selenium runs your browser and opens webpage
-driver = webdriver.Chrome(options=chrome_options)
-driver.implicitly_wait(2)
-driver.get('https://tradingeconomics.com/calendar')
-# Sets java script variables and executes them on page
-calendar_range = 'setCalendarRange("3")'
-calendar_importance = 'setCalendarImportance("3")'
-driver.execute_script(calendar_range)
-driver.execute_script(calendar_importance)
-# Extracts source code and makes a Beautiful Soup object
-html = driver.page_source
-soup = BeautifulSoup(html, 'lxml')
-# Extracts all inner html text of headline info
-calendar_events = soup.find_all('a', class_='calendar-event')
-# writes a text file of list of macro-economic events taking place this week
-with open('events.txt', 'w') as events:
-    for n in calendar_events:
-        events.write(n.get_text() + '\n')
-# Selenium closes browser instance
-driver.quit()
+    with open('events.txt', 'w') as events:
+        for n in calendar_events:
+            events.write(n.get_text() + '\n')
+    driver.quit()
